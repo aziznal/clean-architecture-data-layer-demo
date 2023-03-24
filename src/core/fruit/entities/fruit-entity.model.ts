f30dab895f8b99bd7ebfd27ae71a99130fbefa1c
Aziz <aziznal.dev@gmail.com>
2023-03-24 11:17:15 +0300

    Setup fruit grid with styles

--- src/core/fruit/entities/fruit-entity.model.ts
+++ src/core/fruit/entities/fruit-entity.model.ts
@@ -1,6 +1,7 @@
 export interface FruitEntity {
   id: string;
   name: string;
+  description: string;
   pricePerKilo: number;
   thumbnailUrl: string;
 }
--- src/core/fruit/usecases/get-fruits-usecase.ts
+++ src/core/fruit/usecases/get-fruits-usecase.ts
@@ -16,23 +16,34 @@ export class GetFruitsUseCaseImpl implements GetFruitsUseCase {
       {
         id: '1',
         name: 'Apple',
-        pricePerKilo: 100,
+        description: 'This is an apple',
+        pricePerKilo: 1,
         thumbnailUrl:
-          'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+          'https://www.collinsdictionary.com/images/full/apple_158989157.jpg',
       },
       {
         id: '2',
         name: 'Orange',
-        pricePerKilo: 200,
+        description: 'This is an orange',
+        pricePerKilo: 2,
         thumbnailUrl:
-          'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+          'https://i5.walmartimages.ca/images/Enlarge/234/6_r/6000191272346_R.jpg',
       },
       {
         id: '3',
         name: 'Banana',
-        pricePerKilo: 300,
+        description: 'This is a banana',
+        pricePerKilo: 2.5,
         thumbnailUrl:
-          'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+          'https://th-thumbnailer.cdn-si-edu.com/xK6NAJHiv_51fzn5sDiQt0eD5Is=/fit-in/1600x0/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/d5/24/d5243019-e0fc-4b3c-8cdb-48e22f38bff2/istock-183380744.jpg',
+      },
+      {
+        id: '4',
+        name: 'Pineapple',
+        description: 'This is a pineapple',
+        pricePerKilo: 5,
+        thumbnailUrl:
+          'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71+qAJehpkL.jpg',
       },
     ];
   }
