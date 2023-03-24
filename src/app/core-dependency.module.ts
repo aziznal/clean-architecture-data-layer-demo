6e73ca5381478ff27cd51fa1f0827eebe4eae04d
Aziz <aziznal.dev@gmail.com>
2023-03-24 11:30:18 +0300

    Setup DI for create-fruit usecase and move repo to data-di module

--- src/app/core-dependency.module.ts
+++ src/app/core-dependency.module.ts
@@ -1,23 +1,28 @@
-import { FruitRepositoryImpl } from './../data/fruit/fruit-repository-impl';
 import { NgModule, Provider } from '@angular/core';
 
 import { FruitRepository } from '../core/fruit/fruit-repository';
 import {
+  CreateFruitUseCase,
+  CreateFruitUseCaseImpl,
+} from '../core/fruit/usecases/create-fruit-usecase';
+import {
   GetFruitsUseCase,
   GetFruitsUseCaseImpl,
 } from '../core/fruit/usecases/get-fruits-usecase';
+import { FruitRepositoryImpl } from '../data/fruit/fruit-repository-impl';
 
 const providers: Provider[] = [
-  {
-    provide: FruitRepository,
-    useFactory: () => {
-      return new FruitRepositoryImpl();
-    },
-  },
   {
     provide: GetFruitsUseCase,
     useFactory: (fruitRepository: FruitRepository) => {
       return new GetFruitsUseCaseImpl(fruitRepository);
+    },
+    deps: [FruitRepository],
+  },
+  {
+    provide: CreateFruitUseCase,
+    useFactory: (fruitRepository: FruitRepository) => {
+      return new CreateFruitUseCaseImpl(fruitRepository);
     },
     deps: [FruitRepository],
   },
--- src/app/data-dependency.module.ts
+++ src/app/data-dependency.module.ts
@@ -1,7 +1,16 @@
+import { FruitRepositoryImpl } from './../data/fruit/fruit-repository-impl';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FruitRepository } from '../core/fruit/fruit-repository';
 
 @NgModule({
-  providers: [],
+  providers: [
+    {
+      provide: FruitRepository,
+      useFactory: () => {
+        return new FruitRepositoryImpl();
+      },
+    },
+  ],
 })
 export class DataDependencyModule {}
